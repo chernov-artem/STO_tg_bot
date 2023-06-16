23bc840b545c3fa1db95c95b1a9f8a01a1640705
chernov-artem <9705947@gmail.com>
2023-06-16 08:31:13 +0300

    создана таблица с заказами добавлена функция создания этой таблицы таблица заказов добавлена в функция дропа таблиц

--- db_functions.py
+++ db_functions.py
@@ -7,9 +7,19 @@ def create_table():
     '''Создает таблицу в БД, если она ещё не создана'''
     sql.execute("""CREATE TABLE IF NOT EXISTS clients(
     id INTEGER primary key,
-    name TEXT)""")
+    client_name TEXT)""")
     db.commit()
     print('таблица создана')
+
+def create_table_orders():
+    '''Создает таблицу в БД, если она ещё не создана'''
+    sql.execute("""CREATE TABLE IF NOT EXISTS orders(
+    client_name TEXT,
+    order_time TEXT)""")
+    db.commit()
+    print('таблица orders создана')
+
+
 def sign_up(name):
     """ функция записи на прием"""
     print(type(name), name)
@@ -22,16 +32,18 @@ def sign_up(name):
 
 def drop_table():
     sql.execute('DROP TABLE clients')
+    sql.execute('DROP TABLE orders')
     db.commit()
-    print('таблица clients дропнута')
+    print('таблица clients и orders дропнуты')
 
-sign_up('vasia')
+# sign_up('vasia')
 # sql.execute("INSERT INTO clients (name) VALUES ('VASIA')")
 # db.commit()
 # sql.execute("SELECT * FROM clients")
 # print(sql.fetchall())
-# drop_table()
+drop_table()
 # create_table()
+# create_table_orders()
 # sign_up('vasia')
 # hui = 'dfwwfr'
 # sql.execute("INSERT INTO clients (name) VALUES (?)", (hui))

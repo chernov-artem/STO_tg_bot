e7acd351e31c20e6fd32c575d7069ea4e2199bf4
chernov-artem <9705947@gmail.com>
2023-06-18 22:44:46 +0300

    добавлена функция refresh

--- db_functions.py
+++ db_functions.py
@@ -3,7 +3,7 @@ import sqlite3
 db = sqlite3.connect("server.db")
 sql = db.cursor()
 
-def create_table_clienst():
+def create_table_clients():
     '''Создает таблицу в БД, если она ещё не создана'''
     sql.execute("""CREATE TABLE IF NOT EXISTS clients(
     id INTEGER primary key,
@@ -16,7 +16,9 @@ def create_table_orders():
     sql.execute("""CREATE TABLE IF NOT EXISTS orders(
     client_name TEXT,
     order_day TEXT,
-    order_time TEXT)""")
+    order_time TEXT,
+    car TEXT,
+    cost INTEGER)""")
     db.commit()
     print('таблица orders создана')
 
@@ -45,7 +47,7 @@ def del_client(name: str):
     db.commit()
     print(f"Клиент {name} удален")
 
-def new_order(name, day, time):
+def new_order(name, day, time, car, cost):
     """ функция записи на прием. Добавляет клиента в базу клиентов, если он записывается в первый раз"""
     sql.execute(f"SELECT * FROM clients WHERE client_name = '{name}'")
     res = sql.fetchall()
@@ -59,7 +61,8 @@ def new_order(name, day, time):
         sql.execute(f"INSERT INTO clients (client_name) VALUES ('{name}')")
         print(f"добавили нового клиента {name}. Записали {day} дня на {time}")
 
-    sql.execute(f"INSERT INTO orders (client_name, order_day, order_time) VALUES ('{name}', '{day}', '{time}')")
+    sql.execute(f"INSERT INTO orders (client_name, order_day, order_time, car, cost)"
+                f" VALUES ('{name}', '{day}', '{time}', '{car}', '{cost}')")
     db.commit()
 
 def show_client_orders(name: str):
@@ -79,24 +82,37 @@ def drop_table():
     sql.execute('DROP TABLE orders')
     db.commit()
     print('таблица clients и orders дропнуты')
-    create_table_clienst()
+    create_table_clients()
     create_table_orders()
 
+def refresh():
+    """ функция обновляет данные в таблицах"""
+    sql.execute("SELECT cost FROM orders WHERE client_name = 'vasia'")
+    res = sql.fetchall()
+    # считаем сумму за все заказы
+    cost_sum = 0
+    for i in res:
+        cost_sum+= i[0]
+    print(res, type(res), cost_sum)
+
+
 # sign_up('masha')
-# new_order('petia', 5, '12:30')
-# new_order('masha', 1, '15:30')
-new_order('vasia', 1, '10:00')
-new_order('vasia', 2, '11:00')
-new_order('vasia', 3, '10:30')
-new_order('vasia', 4, '12:30')
-# new_order('petia', 7, '14:00')
-# new_order('алколеша', 4, '17:30')
-# new_order('алколеша', 4, '17:30')
-# new_order('алколеша', 5, '12:35')
+# new_order('petia', 5, '12:30', 'citro', 11000)
+# new_order('masha', 1, '15:30', 'peogeot 207', 7500)
+# new_order('vasia', 1, '10:00', 'vaz 2106', 1600)
+# new_order('vasia', 2, '11:00', 'vaz 2106', 1200)
+# new_order('vasia', 3, '10:30', 'vaz 2106', 3150)
+# new_order('vasia', 4, '12:30', 'vaz 2106', 2100)
+# new_order('petia', 7, '14:00', 'sub', 14000)
+# new_order('алколеша', 4, '17:30', 'volga', 5000)
+# new_order('алколеша', 4, '17:30', 'UAZ', 12350)
+# new_order('алколеша', 5, '12:35', 'UAZ', 21500)
 # print(order_time_chek(4, '17:30'))
 # del_client_order('vasia', '2', '11:00')
 # show_client_orders('petia')
 # del_client('vasia')
+
+refresh()
 
 # drop_table()
 
